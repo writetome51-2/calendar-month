77c1afb10104aec9d8ea7564b9af6e4b514b25de
Steve Thompson <writetome51@gmail.com>
2024-09-30 23:20:24 -0600

    save

--- lib/on-init-take-focus.directive.ts
+++ lib/on-init-take-focus.directive.ts
@@ -1,12 +1,38 @@
 export class OnInitTakeFocusDirective {
+   //
+   private __returnFocusOnDestroy = true;
+   private __previouslyFocusedElementId: string;
+   private readonly __temporaryId: string = "app-temporary-id-" + Math.random();
 
-   constructor(private host: HTMLElement) {
-   }
+   // input
+   delayReturningFocus = 0; // ms
+
+   constructor(private __host: HTMLElement) {}
 
    onInit() {
-      this.host.focus();
+      if (!document.activeElement.id) {
+         document.activeElement.setAttribute("id", this.__temporaryId);
+      }
+      this.__previouslyFocusedElementId = document.activeElement.id;
+
+      // If user blurs element before it's destroyed, we don't return focus
+      this.__host.onblur = () => (this.__returnFocusOnDestroy = false);
+      this.__host.focus();
    }
 
    onDestroy() {
+      setTimeout(() => {
+         const previouslyFocusedElement = document.getElementById(
+            this.__previouslyFocusedElementId
+         );
+         if (!!previouslyFocusedElement) {
+            if (this.__returnFocusOnDestroy) {
+               previouslyFocusedElement.focus();
+            }
+            if (this.__previouslyFocusedElementId === this.__temporaryId) {
+               previouslyFocusedElement.removeAttribute("id");
+            }
+         }
+      }, this.delayReturningFocus);
    }
 }
